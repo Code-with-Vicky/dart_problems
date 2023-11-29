66915f01b0ed7146492ecdd0631f8a8d9c1a6506
Vignesh <vigneshkumarg18@gmail.com>
2023-11-29 13:21:24 +0530

    High Scores completed

--- high-scores/lib/high_scores.dart
+++ high-scores/lib/high_scores.dart
@@ -1,3 +1,34 @@
 class HighScores {
-  // Put your code here
+  List<int> scores;
+  HighScores(this.scores);
+
+  int latest(){
+    return scores.last;
+  }
+
+  int personalBest(){
+    int max = scores.first;
+    for(int item in scores){
+      if(item > max){
+        max = item;
+      }
+    }
+    return max;
+  }
+
+  List<int> personalTopThree(){
+    List<int> copyScores = [];
+    for(int item in scores){
+      copyScores.add(item);
+    }
+    copyScores.sort((a,b) => b-a);
+    List<int> topThree = [];
+    for(int item in copyScores) {
+      topThree.add(item);
+      if(topThree.length == 3){
+        break;
+      }
+    }
+    return topThree;
+  }
 }
